c69c90cf1f0efd5a029fcefd33a0583dc59964cf
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2017-12-12 11:14:17 -0200

    update spark

--- spark/bootstrap.sh
+++ spark/bootstrap.sh
@@ -3,7 +3,7 @@
 $HADOOP_HOME/bin/hdfs dfs -test -d /spark
 if [ $? != 0 ]
 then
-  $HADOOP_HOME/bin/hdfs dfs -put $SPARK_HOME-1.6.0-bin-hadoop2.6/lib /spark
+  $HADOOP_HOME/bin/hdfs dfs -put $SPARK_HOME-2.2.1-bin-hadoop2.7/lib /spark
 else
   echo "Spark already on HDFS"
 fi
